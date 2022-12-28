90511b81f0b9e225f6f41a5501c111fe87aba21b
coding-jeetu <116790643+coding-jeetu@users.noreply.github.com>
2022-12-28 15:39:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import Header from './Components/Header';
+import Section from './Components/Section';
+import Lastpart from './Components/Last part';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+    <Header />
+    <Section />
+    <Lastpart />
+    </>
+  
+    </div>
+  );
+}
+
+export default App;
